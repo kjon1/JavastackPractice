0e55a6888777ef2a607bedfd1ee1c3655dfad05e
Jong Kim <john.kim@revature.net>
2021-07-27 18:57:25 -0400

    push

---
+++ day2/Demo12/src/com/Jong/Demo12.java
@@ -0,0 +1,17 @@
+package com.Jong;
+
+class Car{
+    public final void drive(){
+        System.out.println("driving....");
+    }
+}
+
+class Honda extends Car{
+
+}
+
+public class Demo12 {
+    public static void main(String[] args) {
+        
+    }
+}
